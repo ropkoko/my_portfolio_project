f7a9150dc02bb284f2ee2fca6ddd335de268b074
ropkoko <ropbrenda4@gmail.com>
2024-10-19 15:17:17 +0300

    tailwind changes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb', // For Rails views
-    './app/helpers/**/*.rb', // For Rails helper files
-    './app/assets/stylesheets/**/*.css', // For CSS files
-    './app/javascript/**/*.js', // For JavaScript files
-    './app/**/*.html' // For HTML files
+    "./app/views/**/*.html.erb", // For Rails views
+    "./app/helpers/**/*.rb", // For Rails helper files
+    "./app/assets/stylesheets/**/*.css", // For CSS files
+    "./app/javascript/**/*.js", // For JavaScript files
+    "./app/**/*.html" // For HTML files
   ],
   theme: {
     extend: {}, // No trailing comma here
