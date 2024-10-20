b614fe7a038dbb9d858b0dd56b6e49ec4045b98e
ropkoko <ropbrenda4@gmail.com>
2024-10-20 21:02:25 +0300

    ruby scan failer so i had to correct it in application.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,28 +1,7 @@
 class Project < ApplicationRecord
-  has_one_attached :cover_image
-  has_rich_text :content
-  belongs_to :user
-
+  # Validations
   validates :title, presence: true
-  validates :content, presence: true
-  validates :user_id, presence: true
+  validates :description, presence: true
 
-  scope :sorted, ->{ order(arel_table[:published_at].desc.nulls_first).order(updated_at: :desc) }
-  scope :draft, -> { where(published_at: nil) }
-  scope :published, -> { where("published_at <= ?", Time.current) }
-  scope :scheduled, -> { where("published_at > ?", Time.current) }
-
-  def draft?
-    published_at.nil?
-  end
-
-  def published?
-    published_at? && published_at <= Time.current
-  end
-
-  def scheduled?
-    published_at? && published_at > Time.current
-  end
-
-end
+  # Other model methods or logic
 end
